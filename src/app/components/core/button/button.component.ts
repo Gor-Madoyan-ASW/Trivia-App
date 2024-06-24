d3fc9180a358ea70b5486e457a716c4d34e015fb
Gor Madoyan <154419869+Gor-Madoyan-ASW@users.noreply.github.com>
2024-06-24 18:34:12 +0400

    Initial commit

--- src/app/components/core/button/button.component.ts
+++ src/app/components/core/button/button.component.ts
@@ -1,5 +1,5 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-button',
